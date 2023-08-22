c9dcf547c7860a86224b5814ba2d7f9b0a8ef099
shivakant <shivakanttiwari123@gmail.com>
2023-08-22 18:52:47 +0530

    create book list , userlist page

--- src/component/dashboard/BookList.js
+++ src/component/dashboard/BookList.js
@@ -2,6 +2,52 @@ const BookList = () =>{
     return(
         <>
         <h2>BookList</h2>
+        <div className="bk_table">
+            <table>
+                <thead>
+                    <tr>
+                        <th>S.No</th>
+                        <th>Book Name</th>
+                        <th>Author Name</th>
+                        <th>Publish Date</th>
+                        <th>Avaibility</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>01</td>
+                        <td>My Battle</td>
+                        <td>matt hannery</td>
+                        <td>12-05-2023</td>
+                        <td>Yes</td>
+                        <td><button>Edit</button>
+                        <button>delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>02</td>
+                        <td>Journay of Tom</td>
+                        <td>Tom Doe</td>
+                        <td>12-05-2023</td>
+                        <td>Yes</td>
+                        <td><button>Edit</button>
+                        <button>delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>03</td>
+                        <td>My Battle</td>
+                        <td>matt hannery</td>
+                        <td>12-05-2023</td>
+                        <td>No</td>
+                        <td><button>Edit</button>
+                        <button>delete</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
         </>
     )
 }
--- src/component/dashboard/UserList.js
+++ src/component/dashboard/UserList.js
@@ -2,6 +2,52 @@ const UserList = () =>{
     return(
         <>
         <h2>UserList</h2>
+        <div className="bk_table">
+            <table>
+                <thead>
+                    <tr>
+                        <th>S.No</th>
+                        <th>User Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>01</td>
+                        <td>Jhon Doe</td>
+                        <td>jhon@gmail.com</td>
+                        <td>1112223331</td>
+                        <td>Active</td>
+                        <td><button>Edit</button>
+                        <button>delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>02</td>
+                        <td>Umesh Henary</td>
+                        <td>uk@gmail.com</td>
+                        <td>3332221111</td>
+                        <td>InActive</td>
+                        <td><button>Edit</button>
+                        <button>delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>03</td>
+                        <td>Monu singh</td>
+                        <td>sing@gmail.com</td>
+                        <td>858585858</td>
+                        <td>Active</td>
+                        <td><button>Edit</button>
+                        <button>delete</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
         </>
     )
 }
