2084cb75187395bb1231e6be7a05cd052f1b285b
shivakant <shivakanttiwari123@gmail.com>
2023-08-23 18:46:34 +0530

    create dashboad all page with responsive

--- src/component/dashboard/Profile.js
+++ src/component/dashboard/Profile.js
@@ -1,9 +1,11 @@
-const Profile = () =>{
-    return(
-        <>
+const Profile = () => {
+  return (
+    <>
+      <div className="dash_heading d-flex">
         <h2>Profile</h2>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Profile;
